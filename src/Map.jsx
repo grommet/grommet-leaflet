dd3389ff6e9e3d374c0c639efa215d812f79f040
Matt Glissmann <mdglissmann@gmail.com>
2023-03-27 08:16:55 -0600

    add marker for user's location

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
-import { Box } from "grommet";
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import { Box } from 'grommet';
 
 function Map() {
   const [geolocation, setGeolocation] = useState();
@@ -8,13 +8,14 @@ function Map() {
   const mapContainerRef = useRef();
 
   const adjustSize = () => {
-    const map = document.getElementById("map");
+    const map = document.getElementById('map');
     if (containerRef.current && map) {
       const height = containerRef.current.getBoundingClientRect().height;
       map.style.height = `${height}px`;
     }
   };
 
+  // get the user's location
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
@@ -23,18 +24,19 @@ function Map() {
           position.coords.longitude,
         ];
         setGeolocation(nextLocation);
-        localStorage.setItem("geolocation", JSON.stringify(nextLocation));
+        localStorage.setItem('geolocation', JSON.stringify(nextLocation));
       },
       () => {
-        const stored = localStorage.getItem("geolocation");
+        const stored = localStorage.getItem('geolocation');
         if (stored) setGeolocation(JSON.parse(stored));
-      }
+      },
     );
   }, []);
 
+  // adjust the map size when the window is resized
   useEffect(() => {
-    window.addEventListener("resize", adjustSize);
-    return () => window.removeEventListener("resize", adjustSize);
+    window.addEventListener('resize', adjustSize);
+    return () => window.removeEventListener('resize', adjustSize);
   });
 
   useLayoutEffect(adjustSize);
@@ -49,14 +51,14 @@ function Map() {
           zoom={6}
           scrollWheelZoom={false}
         >
-          {/* <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          /> */}
           <TileLayer
-            attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+            attribution={`
+              &copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, 
+              &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> 
+              &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors`}
             url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
           />
+          <Marker position={geolocation} />
         </MapContainer>
       )}
     </Box>
