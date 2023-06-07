6e4f30f36c8bb2a717d80744462279de8112de99
Jessica Filben <jessica.filben@gmail.com>
2023-06-07 14:57:38 -0600

    Add sample geojson data

---
+++ src/utils/geojson_data.js
@@ -0,0 +1,75 @@
+export const geojsonLocations = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        name: "Coors Field",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.99404, 39.75621],
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-105.089299, 40.573757],
+      },
+      properties: {
+        name: "Colorado State University",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-105.155528, 40.555576],
+      },
+      properties: {
+        name: "Horsetooth Reservoir",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-105.087239, 40.412659],
+      },
+      properties: {
+        name: "Lake Loveland",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-105.279259, 40.017926],
+      },
+      properties: {
+        name: "Pearl Street",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-104.661638, 39.858828],
+      },
+      properties: {
+        name: "Denver International Airport",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-104.888013, 38.871175],
+      },
+      properties: {
+        name: "Garden of the Gods",
+      },
+    },
+  ],
+};
