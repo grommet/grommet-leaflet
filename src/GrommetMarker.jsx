f2db70c402183d8b1f5a01c2371048ab7afb0e58
Taylor Seamans <taylor.seamans@yahoo.com>
2023-06-09 11:45:46 -0700

    Remove unnnecessary cloneElemennt

--- src/GrommetMarker.jsx
+++ src/GrommetMarker.jsx
@@ -10,17 +10,14 @@ import L from 'leaflet';
 import { Pin } from './markers';
 
 const createGrommetMarker = (
-  { position, title, alt, icon: iconProp, status },
+  { position, title, alt, icon: iconProp },
   context,
 ) => {
   const theme = useContext(ThemeContext)
   const icon = L.divIcon({
     // 'grommet-marker' class prevents leaflet default divIcon styles
     className: 'grommet-marker',
-    html: ReactDOMServer.renderToString(React.cloneElement(iconProp, {
-      // necessary to pass theme prop for StyledBox in Pi
-      theme: theme
-    }) || <Pin status="unknown" theme={theme} />),
+    html: ReactDOMServer.renderToString(iconProp || <Pin status="unknown" theme={theme} />),
   });
   const options = { title, alt, icon };
   const marker = new L.Marker(position, options);
