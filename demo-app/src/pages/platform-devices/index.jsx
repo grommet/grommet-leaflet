b01576b295895d52e7d7baf9da4929879728a987
Matt Glissmann <mdglissmann@gmail.com>
2023-07-25 14:12:49 -0600

    add and format glcp device data

--- demo-app/src/pages/platform-devices/index.jsx
+++ demo-app/src/pages/platform-devices/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Page, PageContent, PageHeader } from 'grommet';
+import { Box, Data, Page, PageContent, PageHeader } from 'grommet';
+import { Controls, GeoJSON, Map, Marker, Pin } from 'grommet-leaflet-core';
 import { PageSection, ReverseAnchor } from '../../components';
+import devices from './data/1073-devices-customer.geojson.json';
+
+const modifedDevices = { ...devices, features: devices.features.slice(0, 3) };
 
 const PlatformDevices = () => {
   return (
@@ -11,7 +15,29 @@ const PlatformDevices = () => {
           title="HPE GreenLake Devices"
           parent={<ReverseAnchor as={Link} label="Home" to="/" />}
         />
-        <PageSection>yo</PageSection>
+        <PageSection>
+          <Data>
+            <Box
+              background="background-contrast"
+              height={{ min: 'medium' }}
+              // skeleton
+            >
+              <Map id="map">
+                {/* <Controls locations={modifedDevices} /> */}
+                {/* <GeoJSON data={modifedDevices} /> */}
+                {modifedDevices
+                  ? modifedDevices.features.map(device => {
+                      console.log(device.geometry.coordinates);
+                      <Marker
+                        position={device.geometry.coordinates}
+                        icon={<Pin />}
+                      />;
+                    })
+                  : null}
+              </Map>
+            </Box>
+          </Data>
+        </PageSection>
       </PageContent>
     </Page>
   );
