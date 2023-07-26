3c585d6e9f5250ca1d0a159b76d844c67f2b2e93
Matt Glissmann <mdglissmann@gmail.com>
2023-07-26 12:09:59 -0600

    add notification for devices without locations

--- demo-app/src/pages/platform-devices/index.jsx
+++ demo-app/src/pages/platform-devices/index.jsx
@@ -1,6 +1,13 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { Box, Data, Page, PageContent, PageHeader } from 'grommet';
+import {
+  Box,
+  Data,
+  Notification,
+  Page,
+  PageContent,
+  PageHeader,
+} from 'grommet';
 import {
   Controls,
   GeoJSON,
@@ -15,6 +22,15 @@ const devicesWithLocation = {
   ...devices,
   features: devices.features.filter(device => {
     if (device.geometry.coordinates[0] !== null) {
+      return device;
+    }
+  }),
+};
+
+const devicesWithoutLocation = {
+  ...devices,
+  features: devices.features.filter(device => {
+    if (device.geometry.coordinates[0] === null) {
       return device;
     }
   }),
@@ -38,27 +54,45 @@ const PlatformDevices = () => {
         />
         <PageSection>
           <Data data={devicesWithLocation.features} toolbar>
-            <Box
-              background="background-contrast"
-              height={{ min: 'medium' }}
-              skeleton={loading}
-            >
-              <Map id="map">
-                <Controls locations={devicesWithLocation} />
-                <GeoJSON data={devicesWithLocation} />
-                <MarkerCluster>
-                  {devicesWithLocation
-                    ? devicesWithLocation.features.map(device => {
-                        return device.geometry.coordinates[0] ? (
-                          <Marker
-                            key={device.properties.serial_number}
-                            position={device.geometry.coordinates}
-                          />
-                        ) : null;
-                      })
-                    : null}
-                </MarkerCluster>
-              </Map>
+            <Box gap="small">
+              {devicesWithoutLocation.features.length > 0 ? (
+                <Notification
+                  status="info"
+                  message={`${new Intl.NumberFormat(navigator.language).format(
+                    devicesWithoutLocation.features.length,
+                  )} devices do not have location specified. ${new Intl.NumberFormat(
+                    navigator.language,
+                  ).format(
+                    devicesWithLocation.features.length,
+                  )} of ${new Intl.NumberFormat(navigator.language).format(
+                    devicesWithLocation.features.length +
+                      devicesWithoutLocation.features.length,
+                  )} devices displayed.`}
+                  actions={[{ label: 'Assign locations', onClick: () => {} }]}
+                />
+              ) : null}
+              <Box
+                background="background-contrast"
+                height={{ min: 'medium' }}
+                skeleton={loading}
+              >
+                <Map id="map">
+                  <Controls locations={devicesWithLocation} />
+                  {/* <GeoJSON data={devicesWithLocation} /> */}
+                  <MarkerCluster>
+                    {devicesWithLocation
+                      ? devicesWithLocation.features.map(device => {
+                          return device.geometry.coordinates[0] ? (
+                            <Marker
+                              key={device.properties.serial_number}
+                              position={device.geometry.coordinates}
+                            />
+                          ) : null;
+                        })
+                      : null}
+                  </MarkerCluster>
+                </Map>
+              </Box>
             </Box>
           </Data>
         </PageSection>
