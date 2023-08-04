cc45bfe8a19b113f14bd8f7ba2276db2a8b4b2ea
Taylor Seamans <taylor.seamans@yahoo.com>
2023-08-04 11:46:31 -0700

    Fix hooks render error (#38)

--- grommet-leaflet-core/src/layers/MarkerCluster.jsx
+++ grommet-leaflet-core/src/layers/MarkerCluster.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from 'styled-components';
 import {
   createElementObject,
@@ -10,46 +10,10 @@ import 'leaflet.markercluster';
 import ReactDOMServer from 'react-dom/server';
 import { Cluster, Popup } from '.';
 
-const createMarkerClusterGroup = (
-  { icon: iconProp, popup: popupProp, ...rest },
-  context,
-) => {
-  const theme = React.useContext(ThemeContext);
-
+const createMarkerClusterGroup = ({ ...rest }, context) => {
   const markerClusterGroup = new L.MarkerClusterGroup({
     showCoverageOnHover: false,
     zoomToBoundsOnClick: false,
-    iconCreateFunction: cluster => {
-      if (popupProp) {
-        const popup = cluster.bindPopup(
-          ReactDOMServer.renderToString(
-            <ThemeContext.Provider value={theme}>
-              <Popup>{popupProp(cluster)}</Popup>
-            </ThemeContext.Provider>,
-          ),
-        );
-
-        cluster.on('click', () => {
-          popup.openPopup();
-        });
-      }
-
-      return L.divIcon({
-        // 'grommet-cluster-group' class prevents leaflet default divIcon styles
-        className: 'grommet-cluster-group',
-        html: ReactDOMServer.renderToString(
-          <ThemeContext.Provider value={theme}>
-            {iconProp ? (
-              React.cloneElement(iconProp(cluster), {
-                cluster,
-              })
-            ) : (
-              <Cluster cluster={cluster} />
-            )}
-          </ThemeContext.Provider>,
-        ),
-      });
-    },
     ...rest,
   });
   return createElementObject(
@@ -67,9 +31,49 @@ const updateMarkerClusterGroup = (instance, props, prevProps) => {
   }
 };
 
-const MarkerCluster = createPathComponent(
+const LeafletMarkerCluster = createPathComponent(
   createMarkerClusterGroup,
   updateMarkerClusterGroup,
 );
+
+const MarkerCluster = ({ icon: iconProp, popup: popupProp, ...rest }) => {
+  const theme = useContext(ThemeContext);
+  return (
+    <LeafletMarkerCluster
+      iconCreateFunction={cluster => {
+        if (popupProp) {
+          const popup = cluster.bindPopup(
+            ReactDOMServer.renderToString(
+              <ThemeContext.Provider value={theme}>
+                <Popup>{popupProp(cluster)}</Popup>
+              </ThemeContext.Provider>,
+            ),
+          );
+
+          cluster.on('click', () => {
+            popup.openPopup();
+          });
+        }
+
+        return L.divIcon({
+          // 'grommet-cluster-group' class prevents leaflet default divIcon styles
+          className: 'grommet-cluster-group',
+          html: ReactDOMServer.renderToString(
+            <ThemeContext.Provider value={theme}>
+              {iconProp ? (
+                React.cloneElement(iconProp(cluster), {
+                  cluster,
+                })
+              ) : (
+                <Cluster cluster={cluster} />
+              )}
+            </ThemeContext.Provider>,
+          ),
+        });
+      }}
+      {...rest}
+    />
+  );
+};
 
 export { MarkerCluster };
