68540ec79b0dc0a201452f5e072ea72bb595d0d8
Jessica Filben <jessica.filben@gmail.com>
2023-08-04 13:04:25 -0600

    add ReverseAnchor and ContentContainer

--- demo-app/src/pages/network-devices/index.jsx
+++ demo-app/src/pages/network-devices/index.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Page, PageContent, PageHeader } from 'grommet';
 import { NetworkMap } from './NetworkMap';
+import { ContentContainer, ReverseAnchor } from '../../components';
 
 const Index = () => {
   return (
-    <Page kind="full" fill>
+    <Page fill pad={{ bottom: 'xlarge' }}>
       <PageContent flex>
-        <PageHeader title="Networks" />
-        <NetworkMap />
+        <PageHeader
+          parent={<ReverseAnchor as={Link} to="/" label="Home" />}
+          title="Networks"
+        />
+        <ContentContainer fill>
+          <NetworkMap />
+        </ContentContainer>
       </PageContent>
     </Page>
   );
