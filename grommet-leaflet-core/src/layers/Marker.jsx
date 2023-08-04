81148b4df5ef663ede48434433ed6e66c729e413
Taylor Seamans <taylor.seamans@yahoo.com>
2023-08-04 11:36:08 -0700

    Fix hooks render error

--- grommet-leaflet-core/src/layers/Marker.jsx
+++ grommet-leaflet-core/src/layers/Marker.jsx
@@ -1,47 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ReactDOMServer from 'react-dom/server';
+import { ThemeContext } from 'styled-components';
 import {
   createElementObject,
   createPathComponent,
   extendContext,
 } from '@react-leaflet/core';
+import { Popup as LeafletPopup } from 'react-leaflet';
 import L from 'leaflet';
-import { ThemeContext } from 'grommet';
 import { Pin, Popup } from '.';
 
-const createGrommetMarker = (
-  { position, icon: iconProp, popup: popupProp, ...rest },
-  context,
-) => {
-  const theme = React.useContext(ThemeContext);
-
-  const icon = L.divIcon({
-    // 'grommet-marker' class prevents leaflet default divIcon styles
-    className: 'grommet-marker',
-    html: ReactDOMServer.renderToString(
-      <ThemeContext.Provider value={theme}>
-        {iconProp || <Pin />}
-      </ThemeContext.Provider>,
-    ),
-  });
-
-  const kind = iconProp?.props?.kind;
+const createGrommetMarker = ({ position, icon, kind, ...rest }, context) => {
   const options = { icon, kind, ...rest };
   const marker = new L.Marker(position, options);
-
-  if (popupProp) {
-    const popup = marker.bindPopup(
-      ReactDOMServer.renderToString(
-        <ThemeContext.Provider value={theme}>
-          <Popup>{popupProp}</Popup>
-        </ThemeContext.Provider>,
-      ),
-    );
-
-    marker.on('click', () => {
-      popup.openPopup();
-    });
-  }
 
   return createElementObject(
     marker,
@@ -55,6 +26,35 @@ const updateGrommetMarker = (instance, props, prevProps) => {
   }
 };
 
-const Marker = createPathComponent(createGrommetMarker, updateGrommetMarker);
+const LeafletMarker = createPathComponent(
+  createGrommetMarker,
+  updateGrommetMarker,
+);
 
+const Marker = ({ children, icon, ...rest }) => {
+  const theme = useContext(ThemeContext);
+  const kind = icon?.props?.kind;
+
+  return (
+    <LeafletMarker
+      icon={L.divIcon({
+        // 'grommet-marker' class prevents leaflet default divIcon styles
+        className: 'grommet-marker',
+        html: ReactDOMServer.renderToString(
+          <ThemeContext.Provider value={theme}>
+            {icon || <Pin />}
+          </ThemeContext.Provider>,
+        ),
+      })}
+      kind={kind}
+      {...rest}
+    >
+      {children && (
+        <LeafletPopup>
+          <Popup>{children}</Popup>
+        </LeafletPopup>
+      )}
+    </LeafletMarker>
+  );
+};
 export { Marker };
