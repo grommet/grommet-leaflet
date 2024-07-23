c889bb7264e59d305c94f0623d1a2341b9552223
Matt Glissmann <mdglissmann@gmail.com>
2024-07-23 14:45:44 -0600

    merge main

---
+++ grommet-leaflet/vite.config.js
@@ -0,0 +1,26 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'grommet-leaflet',
+      fileName: 'grommet-leaflet',
+    },
+    rollupOptions: {
+      // externalize deps that shouldn't be bundled with library
+      external: ['react', 'grommet'],
+      output: {
+        // global variables to use in the UMD build for externalized deps
+        globals: {
+          react: 'React',
+          grommet: 'grommet',
+        },
+      },
+    },
+  },
+});
