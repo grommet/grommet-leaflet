6f17fee032d9417ad38cbd474ade5d6480c95b61
Matt Glissmann <mdglissmann@gmail.com>
2024-07-23 11:49:25 -0600

    updated release-stable

--- grommet-leaflet/tools/release-stable.mjs
+++ grommet-leaflet/tools/release-stable.mjs
@@ -16,7 +16,8 @@ if (process.env.CI) {
     git()
       .clone(repoURL, localFolder)
       .then(() => git(localFolder).checkout(BRANCH))
-      .then(() => deleteAsync([`${localFolder}/**/*`, `${localFolder}/.*`]))
+      .then(() => deleteAsync([`${localFolder}/**/*`]))
+      .then(() => deleteAsync([`${localFolder}/.*`]))
       .then(() => fs.copy(localDist, `${localFolder}/dist`))
       .then(() =>
         packageInfo.forEach(file => fs.copy(file, `${localFolder}/${file}`)),
