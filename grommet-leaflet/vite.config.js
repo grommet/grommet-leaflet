3f55bb5a525848ffd9d9f6064195f69e3eaf3188
Matt Glissmann <mdglissmann@gmail.com>
2024-07-23 15:52:56 -0600

    add rollup plugin for peerDependencies

--- grommet-leaflet/vite.config.js
+++ grommet-leaflet/vite.config.js
@@ -1,10 +1,11 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [peerDepsExternal(), react()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.js'),
