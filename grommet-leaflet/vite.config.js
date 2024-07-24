9b020bb289aed605d72f933c5c3cf922c84feb12
Matthew Glissmann <mdglissmann@gmail.com>
2024-07-24 17:04:36 -0600

    remove yarn workspaces (#129)

--- grommet-leaflet/vite.config.js
+++ grommet-leaflet/vite.config.js
@@ -2,11 +2,10 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import commonjs from '@rollup/plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [peerDepsExternal(), react(), commonjs()],
+  plugins: [peerDepsExternal(), react()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.js'),
