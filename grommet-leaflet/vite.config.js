8f978496923606e624948a757f78838b215081a6
Matt Glissmann <mdglissmann@gmail.com>
2024-07-24 16:15:58 -0600

    rollup-config

--- grommet-leaflet/vite.config.js
+++ grommet-leaflet/vite.config.js
@@ -2,16 +2,16 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import commonjs from '@rollup/plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [peerDepsExternal(), react()],
+  plugins: [peerDepsExternal(), react(), commonjs()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.js'),
       name: 'grommet-leaflet',
       fileName: 'grommet-leaflet',
-      formats: ['es', 'cjs'],
     },
     rollupOptions: {
       // externalize deps that shouldn't be bundled with library
