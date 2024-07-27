8c5b1cfe621fb9f9e4c60652282c06186d879ea1
Matthew Glissmann <mdglissmann@gmail.com>
2024-07-26 18:10:17 -0600

    styled-components (#149)

--- grommet-leaflet/src/layers/Pin/my-styled-components.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export * from 'styled-components';
-
-const defaultStyled = typeof styled === 'function' ? styled : styled.default;
-
-// eslint-disable-next-line no-restricted-exports
-export { defaultStyled as default };
