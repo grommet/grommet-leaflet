74445f06514b2000161b691119d4f1826d690f30
Matthew Glissmann <mdglissmann@gmail.com>
2024-07-26 18:24:35 -0600

    Bundler 7 (#152)
    * styled-components
    
    * styled-components
    
    * config

--- grommet-leaflet/vite.config.js
+++ grommet-leaflet/vite.config.js
@@ -6,9 +6,6 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [peerDepsExternal(), react()],
-  resolve: {
-    alias: { 'styled-components': require.resolve('styled-components') },
-  },
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.js'),
