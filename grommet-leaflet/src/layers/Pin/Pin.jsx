b17bae3fd03b8a10d58b7ed40079eefce93fb308
Matthew Glissmann <mdglissmann@gmail.com>
2024-07-26 18:21:55 -0600

    Bundler 7 (#151)
    * styled-components
    
    * styled-components

--- grommet-leaflet/src/layers/Pin/Pin.jsx
+++ grommet-leaflet/src/layers/Pin/Pin.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Box } from 'grommet';
-import styled, { ThemeContext } from 'styled-components';
+import { ThemeContext } from 'styled-components';
 import { normalizeTheme } from '../../utils';
 
-const StyledBox = styled(Box)`
-  // to create the pin shape
-  border-bottom-right-radius: 0;
-  // translate to re-align with leaflet div
-  transform: rotate(45deg) translateX(-35%);
-  &:hover {
-    transform: rotate(45deg) translateX(-35%) scale(1.1);
-    transition: transform 0.4s;
-  }
-`;
+// const StyledBox = styled(Box)`
+//   // to create the pin shape
+//   border-bottom-right-radius: 0;
+//   // translate to re-align with leaflet div
+//   transform: rotate(45deg) translateX(-35%);
+//   &:hover {
+//     transform: rotate(45deg) translateX(-35%) scale(1.1);
+//     transition: transform 0.4s;
+//   }
+// `;
 
 const Pin = ({ kind = 'default' }) => {
   const theme = React.useContext(ThemeContext);
