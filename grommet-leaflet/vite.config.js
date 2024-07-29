4f9233b13ef1779ad12e24da98302b117665d86c
Matthew Glissmann <mdglissmann@gmail.com>
2024-07-29 17:09:43 -0600

    add vite plugin for typescript declaration files (#156)

--- grommet-leaflet/vite.config.js
+++ grommet-leaflet/vite.config.js
@@ -2,10 +2,11 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [peerDepsExternal(), react()],
+  plugins: [peerDepsExternal(), react(), dts()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.js'),
