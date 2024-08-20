0c3d269f870e0cee0de43622098ba40605dd6f73
Matthew Glissmann <mdglissmann@gmail.com>
2024-08-20 15:40:47 -0600

    Added support for lat-lng objects (#164)
    * Add initial unit testing
    
    * Added support for latlng objects and initial Controls tests

--- grommet-leaflet/src/layers/Controls/index.d.ts
+++ grommet-leaflet/src/layers/Controls/index.d.ts
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as L from 'leaflet';
 
 export interface ControlsProps {
-  locations?: L.LatLngExpression[];
+  locations?: L.LatLngExpression[] | number[][] | GeoJSON;
 }
 
 declare const Controls: React.FC<ControlsProps>;
