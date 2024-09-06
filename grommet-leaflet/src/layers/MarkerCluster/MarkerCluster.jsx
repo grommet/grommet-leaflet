f611e27adc5b8e3f6f383880446691cd89e22bf1
Matt Glissmann <mdglissmann@gmail.com>
2024-09-06 15:17:47 -0600

    Allow MarkerCluster popup to be interactive

--- grommet-leaflet/src/layers/MarkerCluster/MarkerCluster.jsx
+++ grommet-leaflet/src/layers/MarkerCluster/MarkerCluster.jsx
@@ -2,43 +2,68 @@ import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { ThemeContext } from 'styled-components';
 import {
-  // createElementObject,
-  createPathComponent,
-  // extendContext,
+  createElementObject,
+  createLayerComponent,
+  extendContext,
 } from '@react-leaflet/core';
 import L from 'leaflet';
 import 'leaflet.markercluster';
 import ReactDOMServer from 'react-dom/server';
 import { Cluster, Popup } from '..';
 
-const createElementObject = (instance, context, container) =>
-  Object.freeze({ instance, context, container });
+// const createElementObject = (instance, context, container) =>
+//   Object.freeze({ instance, context, container });
 
-const extendContext = (source, extra) => Object.freeze({ ...source, ...extra });
+// const extendContext = (source, extra) => Object.freeze({ ...source, ...extra });
 
-const createMarkerClusterGroup = ({ ...rest }, context) => {
+const createMarkerClusterGroup = (props, context) => {
+  const clusterProps = {};
+  const clusterEvents = {};
+
+  Object.entries(props).forEach(([key, value]) => {
+    if (key.startsWith('on')) {
+      clusterEvents[key] = value;
+    } else {
+      clusterProps[key] = value;
+    }
+  });
+
   const markerClusterGroup = new L.MarkerClusterGroup({
     showCoverageOnHover: false,
     zoomToBoundsOnClick: false,
-    ...rest,
+    ...clusterProps,
   });
+
+  // Bind events to markerClusterGroup
+  Object.entries(clusterEvents).forEach(([key, value]) => {
+    const event = `cluster${key.substring(2).toLowerCase()}`;
+    markerClusterGroup.on(event, value);
+  });
+
   return createElementObject(
     markerClusterGroup,
     extendContext(context, { layerContainer: markerClusterGroup }),
   );
 };
 
-const LeafletMarkerCluster = createPathComponent(createMarkerClusterGroup);
+const LeafletMarkerCluster = createLayerComponent(createMarkerClusterGroup);
 
-const MarkerCluster = ({ icon: iconProp, popup: popupProp, ...rest }) => {
+const MarkerCluster = ({
+  children,
+  icon: iconProp,
+  popup: popupProp,
+  ...rest
+}) => {
   const theme = useContext(ThemeContext);
 
   return (
     <LeafletMarkerCluster
       iconCreateFunction={cluster => {
+        let popup;
+
         // only bind popup if popupProp is defined
         if (popupProp && popupProp({ cluster })) {
-          const popup = cluster.bindPopup(
+          popup = cluster.bindPopup(
             ReactDOMServer.renderToString(
               <ThemeContext.Provider value={theme}>
                 <Popup>{popupProp({ cluster })}</Popup>
@@ -69,11 +94,14 @@ const MarkerCluster = ({ icon: iconProp, popup: popupProp, ...rest }) => {
         });
       }}
       {...rest}
-    />
+    >
+      {children}
+    </LeafletMarkerCluster>
   );
 };
 
 MarkerCluster.propTypes = {
+  children: PropTypes.node,
   icon: PropTypes.func,
   popup: PropTypes.func,
 };
--- grommet-leaflet/src/layers/index.js
+++ grommet-leaflet/src/layers/index.js
@@ -3,5 +3,7 @@ export * from './Controls';
 export * from './Map';
 export * from './Marker';
 export * from './MarkerCluster';
+export * from './MarkerCluster2';
 export * from './Pin';
 export * from './Popup';
+export * from './TileLayer';
