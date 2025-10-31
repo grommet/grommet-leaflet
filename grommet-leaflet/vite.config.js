5f94bab8abedb01f0587bb0375a7f2765d46657b
Matthew Glissmann <mdglissmann@gmail.com>
2025-10-31 17:59:29 -0600

    bundle adjustments

--- grommet-leaflet/vite.config.js
+++ grommet-leaflet/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
       exclude: ['**/*.test.tsx', '**/*.test.ts', '**/vitest.setup.ts', 'src/index.html'],
       outDir: 'dist',
       entryRoot: 'src',
-    })
+    }),
   ],
   build: {
     lib: {
@@ -28,22 +28,13 @@ export default defineConfig({
       // externalize deps that shouldn't be bundled with library
       external: [
         'react',
-        // 'react/jsx-runtime',
+        'react/jsx-runtime',
         'react-dom',
-        // 'react-dom/server',
+        'react-dom/server',
         'styled-components',
         'grommet',
       ],
       output: {
-        // global variables to use in the UMD build for externalized deps
-        globals: {
-          react: 'React',
-          'styled-components': 'styled',
-          'react-dom': 'ReactDOM',
-          grommet: 'grommet',
-          // 'react-dom/server': 'ReactDOMServer',
-          'react/jsx-runtime': 'react/jsx-runtime.js',
-        },
         interop: 'auto',
       },
     },
