ea0eaae39902fbf7152fdd12a5fa74b1293fee7a
Matthew Glissmann <mdglissmann@gmail.com>
2025-10-31 17:05:08 -0600

    vite config & package.json

--- grommet-leaflet/vite.config.js
+++ grommet-leaflet/vite.config.js
@@ -6,7 +6,16 @@ import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [peerDepsExternal(), react(), dts()],
+  plugins: [
+    peerDepsExternal(), 
+    react(), 
+    dts({
+      include: ['src/**/*.{ts,tsx,js,jsx}'],
+      exclude: ['**/*.test.tsx', '**/*.test.ts', '**/vitest.setup.ts', 'src/index.html'],
+      outDir: 'dist',
+      entryRoot: 'src',
+    })
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.js'),
